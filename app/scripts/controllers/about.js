879395bbeb23454376fdb978baf766c319dc49a7
Jose Nunez <jose.a.nunez@gmail.com>
2014-11-04 13:34:58 -0600

    workable base app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvcloudApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pvcloudApp
+ */
+angular.module('pvcloudApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvcloudApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pvcloudApp
+ */
+angular.module('pvcloudApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
