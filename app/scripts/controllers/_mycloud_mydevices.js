bca2969fc9edb3c34bab5305b7acefed1af37f09
Jose Nunez <jose.nunez@intel.com>
2014-12-09 00:15:11 -0600

    device edition inroads

--- app/scripts/controllers/_mycloud_mydevices.js
+++ app/scripts/controllers/_mycloud_mydevices.js
@@ -3,6 +3,36 @@ angular.module('pvcloudApp').controller('_mycloud_mydevices', function ($scope, 
     console.log($scope);
     getListOfDevicesForAccountID();
     
+    
+    $scope.EditDevice = function (device){
+        var device_id = device.device_id;
+        device.device_nickname_edited = device.device_nickname;
+        device.device_description_edited = device.device_description;
+        device.api_key_edited = device.api_key;
+        device.deviceBeingEdited = true;
+    };
+    
+    $scope.EditDevice_Cancel = function (device){
+        var device_id = device.device_id;
+        
+        device.deviceBeingEdited = false;
+    };
+    
+    $scope.EditDevice_Delete = function (device){
+        var device_id = device.device_id;
+        var response = confirm("De verdad quiere eliminar este dispositivo? Todos sus datos relacionados serán eliminados también.");
+        
+        device.deviceBeingEdited = false;
+    };    
+    
+    $scope.EditDevice_NewAPIKey = function (device){
+        var response = confirm("Desear regenerar el API Key para este dispositivo?");
+        if(response== true){
+            device.api_key = "OK";
+            device.deviceBeingEdited = false;
+        }
+    }
+    
     function getListOfDevicesForAccountID(){
         var token = sessionService.GetCurrentToken();
         var account_id = sessionService.GetCurrentAccountID();
