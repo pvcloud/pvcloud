fe5b10e39d5e1cbf506bc88b84c5c465b468e338
Jose Nunez <jose.nunez@intel.com>
2015-01-30 13:47:38 -0600

    Login/PW Recovery Enhancements

--- app/scripts/controllers/mycloud_login.js
+++ app/scripts/controllers/mycloud_login.js
@@ -2,7 +2,7 @@
 
 angular.module('pvcloudApp').controller('MyCloud_LoginCtrl', function ($scope, AccountService, sessionService, $location, UtilityService, $routeParams) {
     $scope.AllGood = false;
-    if ($location.port() !== "9000") {
+    if ($location.port() !== 9000) {
         if ($location.protocol() !== "https") {
             var currentURL = window.location.href;
             var newURL = currentURL.replace("http", "https").replace(":8080", "");
@@ -11,10 +11,17 @@ angular.module('pvcloudApp').controller('MyCloud_LoginCtrl', function ($scope, A
         } else {
             $scope.AllGood = true;
         }
+    } else {
+        $scope.AllGood = true;
     }
-   
-    //PROCESS AUTHENTICATION SUCESS
-    if($routeParams.account_id>0 && $routeParams.token !== "" && $routeParams.token !== undefined){
+
+    var actionURL = UtilityService.GetBaseURL() + "post_account_login.php";
+    if (actionURL.indexOf("9000")) {
+        document.getElementById("login_form").setAttribute("action", actionURL);
+    }
+
+//PROCESS AUTHENTICATION SUCESS
+    if ($routeParams.account_id > 0 && $routeParams.token !== "" && $routeParams.token !== undefined) {
         var token = $routeParams.token;
         var account_id = $routeParams.account_id;
         var email = $routeParams.email;
@@ -22,30 +29,73 @@ angular.module('pvcloudApp').controller('MyCloud_LoginCtrl', function ($scope, A
         $location.path("mycloud");
         return;
     }
-    
 
     $scope.$parent.ActiveView = "mycloud";
-    $scope.FunctionMode = "LOGIN";//LOGIN or RECOVER_PASSWORD or NEW_ACCOUNT
+    $scope.FunctionMode = "LOGIN"; //LOGIN or RECOVER_PASSWORD or NEW_ACCOUNT
     $scope.Email = "";
     $scope.Nickname = "";
     $scope.Pwd = "";
-    $scope.Pwd2 = "";
     $scope.ErrorMessages = [];
     $scope.LoggedIn = false;
-    generateCapchaCase();
     $scope.GenerateCapchaCase = generateCapchaCase;
+    $scope.RecoverPassword_Click = function () {
+        var email = $scope.Email;
+        AccountService.RequestPasswordRecovery(email);
+        alert("Un mensaje con instrucciones para recuperar su clave se enviará a su cuenta de correo electrónico. Gracias!");
+        $scope.SwitchToLoginMode();
+    };
+    $scope.EnterKeyBehavior = function (event) {
+        if (event.charCode === 13) {
 
-    function hideAddonActions(whatsNext) {
-        $(".pv-credentials-actions").slideUp(200, whatsNext);
-    }
+            switch ($scope.FunctionMode) {
+                case "LOGIN":
+                    $scope.Login();
+                    break;
+                case "RECOVER_PASSWORD":
+                    $scope.RecoverPassword_Click();
+                    break;
+                case "NEW_ACCOUNT":
+                    break;
+            }
 
+        }
+    };
+    
+    //TODO: FUNCTION TO BE REMOVED AFTER CONVERTING TO POST
+    $scope.Login = function () {
+        $scope.ErrorMessages = [];
+        sessionService.Authenticate($scope.Email, $scope.Pwd).$promise.then(function (response) {
+            UtilityService.ProcessServiceResponse(response,
+                    function success(response) {
+                        var token = response.data.token;
+                        var account_id = response.data.account_id;
+                        sessionService.SetToken(token, $scope.Email, account_id);
+                        $location.path("mycloud");
+                    },
+                    function error(response) {
+                        console.log("ERROR");
+                        $scope.ErrorMessages.push(response.message);
+                        console.log($scope.ErrorMessages);
+                    },
+                    function exception(response) {
+                        console.log("EXCEPTION");
+                        $scope.ErrorMessages.push("Ocurrió un error en el servidor. Por favor intente de nuevo más tarde.");
+                    });
+        });
+    };
+    
+    
+    $scope.SwitchToPasswordRecoveryMode = switchToPWRecoveryMode;
+    $scope.SwitchToLoginMode = switchToLoginMode;
+    switchToLoginMode();
+    generateCapchaCase();
+    
     function generateCapchaCase() {
         var num1 = Math.floor((Math.random() * 10) + 1);
         var num2 = Math.floor((Math.random() * 10) + 1);
         var oper = Math.floor((Math.random() * 3) + 1);
         var result = 0;
         var message = "";
-
         switch (oper) {
             case 1: //+
                 result = num1 + num2;
@@ -72,143 +122,16 @@ angular.module('pvcloudApp').controller('MyCloud_LoginCtrl', function ($scope, A
             Message: message,
             Result: result
         };
-
         $scope.CapchaResponse = "";
-
-
     }
 
-    $scope.CheckFields_AddAccount = function () {
-        $scope.ErrorMessages = [];
-        if (!$scope.Email) {
-            $scope.ErrorMessages.push("El campo Email es inválido");
-        }
-
-        if (!$scope.Pwd) {
-            $scope.ErrorMessages.push("El campo Password está vacío");
-        } else if ($scope.Pwd != $scope.Pwd2) {
-            $scope.ErrorMessages.push("Los campos de Password no coinciden");
-        }
-    };
-
-    $scope.RegisterAccount = function () {
-        $scope.CheckFields_AddAccount();
-        if ($scope.ErrorMessages.length == 0) {
-            AccountService.AddNewAccount($scope.Email, $scope.Nickname, $scope.Pwd).$promise.then(
-                    function (response) {
-                        $scope.ErrorMessages = [];
-                        UtilityService.ProcessServiceResponse(response, function () {
-                            $location.path("account_add_complete");
-                        });
-                    },
-                    function (response) {
-                        $scope.ErrorMessages = ["En este momento no podemos crear su cuenta. Por favor intente nuevamente en unos minutos."];
-                    });
-        }
-    };
-
-    $scope.RecoverPassword_Click = function () {
-        var email = $scope.Email;
-        AccountService.RequestPasswordRecovery(email);
-        alert("Un mensaje con instrucciones para recuperar su clave se enviará a su cuenta de correo electrónico. Gracias!");
-        $scope.SwitchToLoginMode();
-    };
-
-    $scope.EnterKeyBehavior = function (event) {
-        if (event.charCode === 13) {
-
-            switch ($scope.FunctionMode) {
-                case "LOGIN":
-                    $scope.Login();
-                    break;
-                case "RECOVER_PASSWORD":
-                    $scope.RecoverPassword_Click();
-                    break;
-                case "NEW_ACCOUNT":
-                    break;
-            }
-
-        }
-    };
-
-    $scope.Login = function () {
-        $scope.ErrorMessages = [];
-        sessionService.Authenticate($scope.Email, $scope.Pwd).$promise.then(function (response) {
-            UtilityService.ProcessServiceResponse(response,
-                    function success(response) {
-                        var token = response.data.token;
-                        var account_id = response.data.account_id;
-                        sessionService.SetToken(token, $scope.Email, account_id);
-                        $location.path("mycloud");
-                    },
-                    function error(response) {
-                        console.log("ERROR");
-                        $scope.ErrorMessages.push(response.message);
-                        console.log($scope.ErrorMessages);
-                    },
-                    function exception(response) {
-                        console.log("EXCEPTION");
-                        $scope.ErrorMessages.push("Ocurrió un error en el servidor. Por favor intente de nuevo más tarde.");
-                    });
-        });
-    };
-
-    function showAddonActions() {
-        $(".pv-credentials-actions").fadeIn(200);
+    function switchToLoginMode() {
+        $("#recover_password_form").slideUp(100);
+        $("#login_form").slideDown(100);
     }
 
-    $scope.SwitchToRegistrationMode = function () {
-        $scope.FunctionMode = "NEW_ACCOUNT";//LOGIN or RECOVER_PASSWORD or NEW_ACCOUNT    
-        $scope.ErrorMessages = [];
-        hideAddonActions(function () {
-            $("#pv-header-password-recovery").slideUp(100);
-            $("#pv-header-credentials").slideUp(100);
-            $(".pv-control-register-link").slideUp(100);
-            $(".pv-control-login").slideUp(100);
-            $(".pv-control-recover-password").slideUp(100, function () {
-                $(".pv-control-back-to-login-separator").slideDown(100);
-                $(".pv-control-recover-password-link").slideDown(100);
-                $("#pv-header-registration").slideDown(100);
-                $(".pv-control-register").slideDown(100);
-                $(".pv-control-back-to-login-link").slideDown(100);
-                $(".pv-input-password2").slideDown(100, function () {
-                    showAddonActions();
-                });
-            });
-        });
-    };
-
-    $scope.SwitchToPasswordRecoveryMode = function () {
-        $scope.FunctionMode = "RECOVER_PASSWORD";//LOGIN or RECOVER_PASSWORD or NEW_ACCOUNT 
-        $scope.ErrorMessages = [];
-        generateCapchaCase();
-
-        hideAddonActions(function () {
-            $(".pv-control-back-to-login-separator").slideUp(100);
-            $(".pv-control-register").slideUp(100);
-            $("#pv-header-registration").slideUp(100);
-            $("#pv-header-credentials").slideUp(100);
-            $(".pv-control-login").slideUp(100);
-            $(".pv-input-password").slideUp(100);
-            $(".pv-input-password2").slideUp(100);
-            $(".pv-control-recover-password-link").slideUp(100, function () {
-                $(".pv-control-register-link").slideDown(100);
-                $("#pv-header-password-recovery").slideDown(100);
-                $(".pv-control-recover-password").slideDown(100);
-                $(".pv-control-back-to-login-link").slideDown(100, showAddonActions);
-            });
-        });
-    };
-
-    $scope.SwitchToLoginMode = function () {
-        $scope.FunctionMode = "LOGIN";//LOGIN or RECOVER_PASSWORD or NEW_ACCOUNT 
-        $scope.ErrorMessages = [];
-        hideAddonActions(function () {
-            $(".pv-control-back-to-login-separator, #pv-header-registration,#pv-header-password-recovery, .pv-control-recover-password, .pv-control-back-to-login-link, .pv-control-register, .pv-input-password2 ").slideUp(200, function () {
-                $(".pv-control-recover-password-link,.pv-input-password, .pv-control-login, .pv-control-register-link, #pv-header-credentials ").slideDown(200, showAddonActions);
-            });
-        });
-
-    };
-
+    function switchToPWRecoveryMode() {
+        $("#recover_password_form").slideDown(100);
+        $("#login_form").slideUp(100);
+    }
 });
