3d2d492ad5a6d178c1efa8cdb10dea3d633f4f29
Jose Nunez <jose.nunez@intel.com>
2015-02-27 20:39:37 -0600

    some UI enhancements and theming

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,18 +1,64 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name pvcloudApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the pvcloudApp
- */
-angular.module('pvcloudApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.$parent.ActiveView = "main";
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('pvcloudApp').controller('MainCtrl', function ($scope, UtilityService, AccountService) {
+
+    $scope.SwitchToPasswordRecoveryMode = switchToPWRecoveryMode;
+
+    $scope.RecoverPassword_Click = function () {
+        if (!$scope.Email)
+            return;
+        var email = $scope.Email;
+        AccountService.RequestPasswordRecovery(email);
+        alert("Un mensaje con instrucciones para recuperar su clave se enviará a su cuenta de correo electrónico. Gracias!");
+        $scope.SwitchToLoginMode();
+    };
+
+    $scope.EnterKeyBehavior = function (event) {
+        if (event.charCode === 13) {
+            switch ($scope.FunctionMode) {
+                case "RECOVER_PASSWORD":
+                    $scope.RecoverPassword_Click();
+                    break;
+            }
+        }
+    };
+
+    $scope.SwitchToPasswordRecoveryMode = switchToPWRecoveryMode;
+
+    $scope.SwitchToLoginMode = switchToLoginMode;
+    $scope.SwitchToNewAccountInfoMode = switchToNewAccountInfoMode;
+
+    function fixLoginFormAction() {
+        var actionURL = UtilityService.GetBackendBaseURL() + "post_account_login.php";
+        console.log(actionURL);
+        if (actionURL.indexOf("9000")) {
+            document.getElementById("login_form").setAttribute("action", actionURL);
+        }
+    }
+
+
+    function switchToPWRecoveryMode() {
+        $("#recover_password_form").slideDown(100);
+        $("#login_form").slideUp(100);
+        $("#email").focus();
+        $scope.FunctionMode = "RECOVER_PASSWORD";
+    }
+
+    function switchToLoginMode() {
+        $("#recover_password_form").slideUp(100);
+        $("#newAccountPanel").slideUp(100);
+        $("#login_form").slideDown(100);
+        $("#username").focus();
+        $scope.FunctionMode = "LOGIN";
+    }
+    
+    function switchToNewAccountInfoMode(){
+        $("#login_form").slideUp(100, function(){
+            $("#newAccountPanel").slideDown(1000);
+        });
+
+    }
+
+    fixLoginFormAction();
+
+});
