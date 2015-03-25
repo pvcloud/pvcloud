aff740d50ff3079c4bd20e254a6642cb781c90af
Jose Nunez <jose.nunez@intel.com>
2015-03-25 16:51:53 -0600

    App basic CRUD

--- app/scripts/controllers/_mycloud_myapps_edit.js
+++ app/scripts/controllers/_mycloud_myapps_edit.js
@@ -1,4 +1,4 @@
-angular.module('pvcloudApp').controller('_mycloud_myapps_edit', function ($scope, UtilityService, AppRegistryService, sessionService, $routeParams) {
+angular.module('pvcloudApp').controller('_mycloud_myapps_edit', function ($scope, UtilityService, AppRegistryService, sessionService, $routeParams, $location) {
     console.log("This is _mycloud_myapps_edit controller being invoked");
     $scope.Tabs = {
         Basics: 'basics',
@@ -6,14 +6,173 @@ angular.module('pvcloudApp').controller('_mycloud_myapps_edit', function ($scope
         Pages: 'pages',
         Data: 'data'
     };
-    
-    $scope.CleanForm = true;
 
+    $scope.FormIsClean = true;
+
+    $scope.SetFormDirty = function () {
+        $scope.FormIsClean = false;
+    };
+
+    $scope.Cancelar = function () {
+        if (!$scope.FormIsClean) {
+            if (!confirm("All your unsaved changes will be lost. Do you want to continue?")) {
+                return;
+            }
+        }
+        $location.path("/mycloud/myapps");
+    };
+
+    $scope.SaveApp = function () {
+
+        $scope.ValidationError_Name = "";
+        $scope.ValidationError_Description = "";
+
+
+        var appToSave = {
+            app_id: $scope.ApplicationID,
+            app_nickname: $scope.AppName,
+            app_description: $scope.AppDescription,
+            api_key: $scope.AppAPIKEY,
+            visibility_type_id: $scope.AppVisibility
+        };
+
+        if (appToSave.app_id !== undefined && appToSave.app_id > 0) {
+            updateApp(appToSave);
+        } else {
+            createApp(appToSave);
+        }
+
+
+        console.log(appToSave);
+
+    };
+
+    $scope.DeleteApp = function () {
+
+        var app_id = $scope.Application.app_id;
+        var confirmation = confirm("De verdad quiere eliminar esta app?\n Todos sus datos relacionados serán eliminados también.");
+        if (confirmation) {
+            var account_id = sessionService.GetCurrentAccountID();
+            var token = sessionService.GetCurrentToken();
+            AppRegistryService.DeleteApp(account_id, token, app_id).$promise.then(function (response) {
+                UtilityService.ProcessServiceResponse(response,
+                        function success(response) {
+                            alert("App #" + app_id + " fue removida satisfactoriamente");
+                            $location.path("/mycloud/myapps");
+                        },
+                        function error(response) {
+                            console.log(response);
+                        },
+                        function exception(response) {
+                            console.log(response);
+                        });
+            });
+            app.appBeingEdited = false;
+        }
+
+
+        console.log(appToSave);
+
+    };
+
+    $scope.RecreateAPIKEY = function () {
+        if (confirm("¿Está seguro que desea continuar?\nEste paso no tiene vuelta atrás. Al regenerar el API KEY todos los dispositivos y sistemas conectados a su APP dejarán de funcionar hasta que descargue nuevamente el controlador correspondiente en cada uno o actualice cada uno con el nuevo API KEY")) {
+
+            var account_id = sessionService.GetCurrentAccountID();
+            var token = sessionService.GetCurrentToken();
+            var app_id = $scope.Application.app_id;
+
+            AppRegistryService.RegenerateAPIKey(account_id, token, app_id).$promise.then(function (response) {
+                UtilityService.ProcessServiceResponse(response,
+                        function success(response) {
+                            loadAppToForm(response.data);
+                            setTimeout(function () {
+                                alert("El API KEY de esta aplicación ha sido regenerado.");
+                            }, 500);
+
+                        },
+                        function error(response) {
+                            console.log(response);
+                        },
+                        function exception(response) {
+                            console.log(response);
+                        });
+            });
+
+        }
+    };
 
     checkForDevelopmentRedirection();
 
     initialize();
 
+    function createApp(appToAdd) {
+        if (appToAdd.app_nickname !== undefined && appToAdd.app_nickname !== "") {
+            if (appToAdd.app_description !== undefined && appToAdd.app_description !== "") {
+                var account_id = sessionService.GetCurrentAccountID();
+                var token = sessionService.GetCurrentToken();
+
+                AppRegistryService.RegisterNewApp(
+                        account_id,
+                        token,
+                        appToAdd.app_nickname,
+                        appToAdd.app_description,
+                        appToAdd.visibility_type_id
+                        ).$promise.then(function (response) {
+                    UtilityService.ProcessServiceResponse(response,
+                            function success(response) {
+                                var app = response.data;
+                                loadAppToForm(app);
+                                alert("Los datos se almacenaron satisfactoriamente.");
+                            },
+                            function error(response) {
+                                console.log(response);
+                            },
+                            function exception(response) {
+                                console.log(response);
+                            });
+                });
+            } else {
+                $scope.ValidationError_Description = "El campo de Descripción es requerido y está vacío.";
+            }
+        } else {
+            $scope.ValidationError_Name = "El nombre de la applicación es requerido y está vacío";
+        }
+    }
+    function updateApp(appToSave) {
+        if (appToSave.app_nickname !== undefined && appToSave.app_nickname !== "") {
+            if (appToSave.app_description !== undefined && appToSave.app_description !== "") {
+                var account_id = sessionService.GetCurrentAccountID();
+                var token = sessionService.GetCurrentToken();
+
+                AppRegistryService.UpdateApp(
+                        account_id,
+                        token,
+                        appToSave.app_id,
+                        appToSave.app_nickname,
+                        appToSave.app_description,
+                        appToSave.visibility_type_id
+                        ).$promise.then(function (response) {
+                    UtilityService.ProcessServiceResponse(response,
+                            function success(response) {
+                                var app = response.data;
+                                loadAppToForm(app);
+                                alert("Los datos se almacenaron satisfactoriamente.");
+                            },
+                            function error(response) {
+                                console.log(response);
+                            },
+                            function exception(response) {
+                                console.log(response);
+                            });
+                });
+            } else {
+                $scope.ValidationError_Description = "El campo de Descripción es requerido y está vacío.";
+            }
+        } else {
+            $scope.ValidationError_Name = "El nombre de la applicación es requerido y está vacío";
+        }
+    }
 
     function initialize() {
         $scope.ArticleID = $routeParams.article_id;
@@ -23,16 +182,11 @@ angular.module('pvcloudApp').controller('_mycloud_myapps_edit', function ($scope
             var account_id = sessionService.GetCurrentAccountID();
             $scope.ApplicationID = $routeParams.article_id;
             AppRegistryService.GetAppByID(account_id, token, $scope.ApplicationID).$promise.then(function (response) {
-                $scope.Application = response.data;
-                console.log($scope.Application);
-                $scope.AppName = $scope.Application.app_nickname;
-                $scope.AppDescription = $scope.Application.app_description;
-                $scope.AppAPIKEY = $scope.Application.api_key;
-                $scope.AppVisibility = $scope.Application.visibility_type_id;
-
+                loadAppToForm(response.data);
             });
         } else {
             $scope.ApplicationID = undefined;
+            $scope.AppVisibility = 1;
         }
 
         $scope.CurrentTab = $scope.Tabs.Basics;
@@ -40,6 +194,17 @@ angular.module('pvcloudApp').controller('_mycloud_myapps_edit', function ($scope
         $scope.SwitchToTab = function (tab) {
             $scope.CurrentTab = tab;
         };
+    }
+
+    function loadAppToForm(app) {
+        $scope.Application = app;
+        $scope.ApplicationID = app.app_id;
+        $scope.AppName = app.app_nickname;
+        $scope.AppCurrentName = app.app_nickname;
+        $scope.AppDescription = app.app_description;
+        $scope.AppAPIKEY = app.api_key;
+        $scope.AppVisibility = app.visibility_type_id;
+        $scope.FormIsClean = true;
     }
 
     function checkForDevelopmentRedirection() {
--- app/scripts/services/appsRegistryService.js
+++ app/scripts/services/appsRegistryService.js
@@ -2,31 +2,31 @@
 
 angular.module('pvcloudApp').factory('AppRegistryService', function ($resource, UtilityService) {
     var baseURL = UtilityService.GetBackendBaseURL();
-    var appAddNewItemResource = $resource(baseURL + "app_register.php?account_id=:account_id&token=:token&app_nickname=:app_nickname&app_description=:app_description", {});
+    var appAddNewItemResource = $resource(baseURL + "app_register.php?account_id=:account_id&token=:token&app_nickname=:app_nickname&app_description=:app_description&visibility_type_id=:visibility_type_id", {});
     var appRegistryResource = $resource(baseURL + "app_get_list_by_account.php?account_id=:account_id&token=:token", {});
     var appResource = $resource(baseURL + "app_get_by_id.php?account_id=:account_id&token=:token&app_id=:app_id", {});
     var appRegenerateAPIKeyResource = $resource(baseURL + "app_generate_api_key.php?account_id=:account_id&token=:token&app_id=:app_id", {});
-    var appUpdateResource = $resource(baseURL + "app_modify.php?account_id=:account_id&token=:token&app_id=:app_id&app_nickname=:app_nickname&app_description=:app_description", {});
+    var appUpdateResource = $resource(baseURL + "app_modify.php?account_id=:account_id&token=:token&app_id=:app_id&app_nickname=:app_nickname&app_description=:app_description&visibility_type_id=:visibility_type_id", {});
     var appDeleteResource = $resource(baseURL + "app_delete.php?account_id=:account_id&token=:token&app_id=:app_id", {});
 
     function getAppListForAccountID(account_id, token) {
         return appRegistryResource.get({account_id: account_id, token: token});
     }
-    
-    function getAppByID(account_id, token, app_id) {
-        return appResource.get({account_id: account_id, token: token, app_id:app_id});
-    }    
 
-    function registerNewApp(account_id, token, app_nickname, app_description) {
-        return appAddNewItemResource.get({account_id: account_id, token: token, app_nickname: app_nickname, app_description: app_description});
+    function getAppByID(account_id, token, app_id) {
+        return appResource.get({account_id: account_id, token: token, app_id: app_id});
+    }
+
+    function registerNewApp(account_id, token, app_nickname, app_description, visibility_type_id) {
+        return appAddNewItemResource.get({account_id: account_id, token: token, app_nickname: app_nickname, app_description: app_description, visibility_type_id:visibility_type_id});
     }
 
     function regenerateAPIKey(account_id, token, app_id) {
         return appRegenerateAPIKeyResource.get({account_id: account_id, token: token, app_id: app_id});
     }
 
-    function updateApp(account_id, token, app_id, app_nickname, app_description) {
-        return appUpdateResource.get({account_id: account_id, token: token, app_id: app_id, app_nickname: app_nickname, app_description: app_description});
+    function updateApp(account_id, token, app_id, app_nickname, app_description, visibility_type_id) {
+        return appUpdateResource.get({account_id: account_id, token: token, app_id: app_id, app_nickname: app_nickname, app_description: app_description, visibility_type_id: visibility_type_id});
     }
 
     function deleteApp(account_id, token, app_id) {
