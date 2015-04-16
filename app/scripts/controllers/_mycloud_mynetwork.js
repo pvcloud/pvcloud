b062ee639bd26f848389f5d0b5700e31bbc2c954
Fanny <fanny.sanchez.umana@intel.com>
2015-04-16 11:10:15 -0600

    WIP for My network

---
+++ app/scripts/controllers/_mycloud_mynetwork.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvcloudApp.controller:MycloudMynetworkCtrl
+ * @description
+ * # MycloudMynetworkCtrl
+ * Controller of the pvcloudApp
+ */
+angular.module('pvcloudApp')
+  .controller('_mycloud_mynetwork', function ($scope) {
+    
+    $scope.connections ={"connections":[
+    {"email":"example1@intel.com", "Name":"Jose"},
+    {"email":"example2@intel.com", "Name":"Pedro"},
+    {"email":"example3@intel.com", "Name":"Karla"}
+]}
+    
+    $scope.connections1 ={"connections":[
+    {"email":"example4@intel.com", "Name":"Jorge"},
+    {"email":"example5@intel.com", "Name":"Sofia"},
+    {"email":"example6@intel.com", "Name":"Marta"}
+]}
+    
+   
+        
+
+
+ 
+  });
