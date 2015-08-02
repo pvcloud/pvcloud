371e60ba3d1339c26f19a2bf9e0c5f4cb917edb8
Jose Nunez <jose.nunez@intel.com>
2015-08-01 19:24:24 -0600

    recovering non-mycloud routes such as page prs err

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,6 +17,24 @@ angular.module('pvcloudApp', [
                     .when('/page/:pageId', {
                         templateUrl: 'views/page.html',
                         controller: 'PageController'
+                    })     
+                    .when('/pmchart/', {
+                        templateUrl: 'views/pmchart.html',
+                        controller: 'PMChartController'
+                    })
+                    .when('/pages/', {
+                        templateUrl: 'views/pages.html',
+                        controller: 'PagesCtrl'
+                    })
+                    .when('/passwordrecovery/:account_id/:confirmation_code', {
+                        templateUrl: 'views/passwordrecovery.html',
+                        controller: 'PasswordrecoveryCtrl'
+                    })
+                    .when('/prs/', {
+                        templateUrl: 'views/passwordrecoverysuccess.html'
+                    })
+                    .when('/err/', {
+                        templateUrl: 'views/error.html'
                     })                    
                     .when('/:section', {
                         templateUrl: 'views/mycloud.html',
@@ -33,24 +51,6 @@ angular.module('pvcloudApp', [
                     .when('/:section/:p1/:p2/:p3', {
                         templateUrl: 'views/mycloud.html',
                         controller: 'MyCloudCtrl'
-                    })
-                    .when('/pmchart', {
-                        templateUrl: 'views/pmchart.html',
-                        controller: 'PMChartController'
-                    })
-                    .when('/pages', {
-                        templateUrl: 'views/pages.html',
-                        controller: 'PagesCtrl'
-                    })
-                    .when('/passwordrecovery/:account_id/:confirmation_code', {
-                        templateUrl: 'views/passwordrecovery.html',
-                        controller: 'PasswordrecoveryCtrl'
-                    })
-                    .when('/prs', {
-                        templateUrl: 'views/passwordrecoverysuccess.html'
-                    })
-                    .when('/err', {
-                        templateUrl: 'views/error.html'
                     })
                     .otherwise({
                         redirectTo: '/'
