b7ae0ccf5bee6dfd986f908216656b0c404adfcf
janunezc <janunezc@janunezc-MOBL3.amr.corp.intel.com>
2015-10-10 15:41:21 -0600

    UNTESTED - Added back "legacy" API functions for backwards compatibility/

--- pvcloud_arduino_library/pvcloud_lib.h
+++ pvcloud_arduino_library/pvcloud_lib.h
@@ -1,10 +1,10 @@
 /*
-  pvcloud_lib.h - Library for pvcloyud
+  pvcloud_lib.h - Library for pvcloud
   Created by Jose Nunez, July 18 2015
   Copyright 2015(c) Intel Corporation
   MIT License
  */
-#include "Arduino.h"
+#include <Arduino.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,19 +12,44 @@
 
 class PVCloud {
 public:
-    void WriteAsync(String, String);
+
+    /*SIMPLIFIED LIBRARTY ASYNC INTERFACE*/
+    void WriteAsync(String label, String value);
     void WriteAsync(String, int);
     void WriteAsync(String, float);
     void WriteAsync(String, double);
-
     void ReadAsync(String);
-    void FileCleanup(String label);
-
     String Check(String);
-    String Check2(String);
 
+    /*LEGACY LOG INTERFACE*/
     void LogEntry(String message);
     void LogEntry(double value);
+
+    /*LEGACY PVCLOUD INTERFACE*/
+    String ReadFileValue();
+
+    void SendString(String label, String value);
+
+    void SendStringNoWait(String label, String value);
+
+    void SendFloat(char *label, float value);
+
+    void SendFloatNoWait(char *label, float value);
+
+    String RetrieveStringValue(String label);
+
+    float RetrieveFloatValue(String label);
+    
+    long RetrieveLongValue(String label);
+    
+    int RetrieveIntValue(String label);
+
+    void RetrieveString_NoWait(String label);
+
+    String RetrieveString_CheckResult(String label);
+    
+    inline bool exists_test3(String name);
+
 
 };
 
