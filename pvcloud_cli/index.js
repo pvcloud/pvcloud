ca584c5eb021db02a48c2e4fe50edda8608a44bc
Jose Nunez <jose.nunez@intel.com>
2015-12-09 16:24:04 -0600

    some structure to command line utility

--- pvcloud_cli/index.js
+++ pvcloud_cli/index.js
@@ -1,4 +1,46 @@
 #! /usr/bin/env node
 
-console.log("HELLO! THIS IS PVCLOUD_CLI");
+(function () {
+    var pvCloudAPI = function () {
+        if (process.argv.length > 2) {
+            executeAsCommandLine();
+        } else {
+            executeAsJSAPI();
+        }
+
+        function executeAsCommandLine() {
+            var action = process.argv[2];
+            switch(action){
+                case "write":
+                    write();
+                    break;
+                case "read":
+                    read();
+                    break;
+                default:
+                    console.log("Invalid action '" + action + "' requested.");
+                    
+            }
+        }
+        
+        function executeAsJSAPI(){
+            //So far nothing should happen here...
+        }
+
+        function write(label, value) {
+            console.log("THIS IS WRITE FUNCTION");
+        }
+
+        function read(label) {
+            console.log("THIS IS READ FUNCTION");
+        }
+
+        return {
+            Write: write,
+            Read: read
+        };
+    }();
+
+    exports.pvCloudAPI = pvCloudAPI;
+})();
 
