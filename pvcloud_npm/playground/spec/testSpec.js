8c9513c71bac4c7459f01d1bd941b50f388df6d9
Jose Nunez <jose.nunez@intel.com>
2016-01-06 19:09:03 -0600

    using just jasmine to do specs testing in node environment

--- pvcloud_npm/playground/spec/testSpec.js
+++ pvcloud_npm/playground/spec/testSpec.js
@@ -1,13 +1,10 @@
-var index = require("./index.js");
-var jasmine = require("jasmine");
+var index = require("../index.js");
 console.log("TEST POINT REACHED");
 console.log(index);
 index.iLib.hello();
 
 describe("iLib Library", function () {
     it("can say hello", function () {
-        index.iLib.hello();
-        
-        expect(1).toBe(1);
+        expect(1).toBe(0);
     });
 });
