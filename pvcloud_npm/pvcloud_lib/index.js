cdfcc50f598a57ab3c26919332d2fe2c67b6d842
Jose Nunez <jose.nunez@intel.com>
2016-01-15 18:25:31 -0600

    LIB TEST THROUGH JASMIN. INCLUDING WRITE ASYNC TEST

--- pvcloud_npm/pvcloud_lib/index.js
+++ pvcloud_npm/pvcloud_lib/index.js
@@ -1,12 +1,327 @@
-require("request");
 (function () {
-    exports.pvcloudAPI = {
-        test: function () {
-            return "THIS WAS JUST A TEST";
-        },
-        write: function () {},
-        read: function () {},
-        post_file: function () {},
-        check: function(){}
+    var request = require("request");
+    var fs = require('fs');
+    var parameters = {
+        async: false
     };
+
+    var DEBUG = false;
+    /**
+     * Wraps the inner call of an HTTP Request
+     * @param {type} url URL to call
+     * @param {type} successCallback Function to call when HTTP Request is successful (Status code 200). Receives parameters response, body, error
+     * @param {type} errorCallback Function to call when HTTP Request is NOT successful (Status code not 200). Receives parameters response, body, error
+     * @param {type} finallyCallback Function to call at the end of the process, passing parameters response, body, error
+     * @returns {undefined}
+     */
+    function requestWrapper(url, successCallback, errorCallback, finallyCallback) {
+        if (parameters.async) {
+            resultToFile(parameters.label, "PVCLOUD_WAITING_FOR_RESPONSE");
+        }
+
+        request(url, function (error, response, body) {
+            if (!error && response && response.statusCode === 200) {
+                log("SUCCESS!!!--------------------------------------------");
+                if (successCallback)
+                    successCallback(response, body, error);
+                else {
+                    if (parameters.async) {
+                        try {
+                            var bodyObject = JSON.parse(body);
+                            var value = bodyObject.vse_value;
+                            OutputResult(value);
+                        } catch (ex) {
+                            errorLog("ASYNC CALL COULD NOT PARSE RESULT");
+                            errorLog(ex);
+                            OutputResult("PVCLOUD_ERROR");
+                        }
+                    } else {
+                        OutputResult(body);
+                    }
+                }
+
+            } else if (response && response.statusCode) {
+                log("WRONG STATUS CODE:---------------------------------------------");
+                log(response.statusCode);
+                log("RESPONSE!!!----------------------------------------------");
+                log(response);
+
+                errorLog("REQUEST FAILED WITH STATUS CODE: " + response.statusCode);
+
+                if (errorCallback) {
+                    errorCallback(response, body, error);
+                } else {
+                    OutputResult("PVCLOUD_ERROR");
+                }
+
+            } else if (error) {
+                log("ERROR:------------------------------------------------");
+                log(error);
+                log("PVCLOUD ERROR PROCESSING:-----------------------------");
+                errorLog(error);
+
+                if (errorCallback)
+                    errorCallback(response, body, error);
+                else {
+                    OutputResult("PVCLOUD_ERROR");
+
+                }
+            } else {
+                log("UNKNOWN FAILURE:---------------------------------------");
+                log(error);
+                errorLog(response);
+
+                if (errorCallback)
+                    errorCallback(response, body, error);
+                else
+                    OutputResult("PVCLOUD_FAILURE");
+            }
+
+            if (finallyCallback)
+                finallyCallback(response, body, error);
+        });
+    }
+
+    function requestWrapperPost(url, successCallback, errorCallback, finallyCallback, label, path, captured_datetime) {
+        log("requestWrapper()");
+        log("URL: ");
+        log(url);
+
+        if (parameters.async) {
+            resultToFile(parameters.label, "PVCLOUD_WAITING_FOR_RESPONSE");
+        }
+
+        var fileRead = fs.createReadStream(path);
+
+        var formData = {
+            vse_label: label,
+            captured_datetime: captured_datetime,
+            app_id: app_id,
+            account_id: account_id,
+            api_key: api_key,
+            fileToUpload: fileRead
+
+        };
+
+        request.post({url: url, formData: formData}, function (error, response, body) {
+            if (!error && response && response.statusCode === 200) {
+                log("SUCCESS!!!--------------------------------------------");
+                if (successCallback)
+                    successCallback(response, body, error);
+                else {
+                    if (parameters.async) {
+                        try {
+                            var bodyObject = JSON.parse(body);
+                            var value = bodyObject.vse_value;
+                            OutputResult(value);
+                        } catch (ex) {
+                            errorLog("ASYNC CALL COULD NOT PARSE RESULT");
+                            errorLog(ex);
+                            OutputResult("PVCLOUD_ERROR");
+                        }
+                    } else {
+                        OutputResult(body);
+                    }
+                }
+
+            } else if (response && response.statusCode) {
+                log("WRONG STATUS CODE:---------------------------------------------");
+                log(response.statusCode);
+                log("RESPONSE!!!----------------------------------------------");
+                log(response);
+
+                errorLog("REQUEST FAILED WITH STATUS CODE: " + response.statusCode);
+
+                if (errorCallback) {
+                    errorCallback(response, body, error);
+                } else {
+                    OutputResult("PVCLOUD_ERROR");
+                }
+
+            } else if (error) {
+                log("ERROR:------------------------------------------------");
+                log(error);
+                log("PVCLOUD ERROR PROCESSING:-----------------------------");
+                errorLog(error);
+
+                if (errorCallback)
+                    errorCallback(response, body, error);
+                else {
+                    OutputResult("PVCLOUD_ERROR");
+
+                }
+            } else {
+                log("UNKNOWN FAILURE:---------------------------------------");
+                log(error);
+                errorLog(response);
+
+                if (errorCallback)
+                    errorCallback(response, body, error);
+                else
+                    OutputResult("PVCLOUD_FAILURE");
+            }
+
+            if (finallyCallback)
+                finallyCallback(response, body, error);
+        });
+    }
+
+    function OutputResult(result) {
+        log("OutputResult---------------------------");
+        log(result);
+        if (parameters.async) { //OUTPUT RESULT IN BASIC LANGUAGE TO FILE
+            resultToFile(parameters.label, result);
+        }
+    }
+
+    function resultToFile(tag, result) {
+        if (!tag)
+            tag = "any";
+
+        var filePath = parameters.async_path || "";
+
+        log("WRITING TO FILE...");
+
+        try {
+            /*
+             var resultFile = filePath + "/out_pvcloud_" + tag + ".txt";
+             log(resultFile);
+             var stream = fs.createWriteStream(resultFile);
+             stream.once('open', function (fd) {
+             try {
+             stream.write(result);
+             stream.end();
+             stream.close();
+             log("FS END REACHED!");
+             } catch (ex) {
+             errorLog(ex);
+             }
+             });
+             */
+            log("END OF resultToFile()");
+        } catch (ex) {
+            errorLog(ex);
+        }
+    }
+
+    /**
+     * Reads content of a result file.
+     * @param {string} tag
+     * @param {function receives (err, data) parameters} callback
+     * @returns {undefined}
+     */
+    function readResultFromFile(tag, callback) {
+        var filePath = parameters.async_path || "";
+        var resultFile = filePath + "/out_pvcloud_" + tag + ".txt";
+        log("READING FROM FILE...");
+        log(resultFile);
+
+        fs.readFile(resultFile, callback);
+    }
+
+    /**
+     * Logs debug data to console if DEBUG is set to true.
+     * @param {String} message
+     * @returns {undefined}
+     */
+    function log(message) {
+        if (DEBUG) {
+            console.log(message);
+        }
+    }
+
+    function errorLog(message) {
+        message = getFormattedDateTime() + " - " + message;
+        message += "\n";
+        log("errorLog()");
+
+        var errorLogFilePath = parameters.error_log_file || "/error_pvcloud.log";
+        log(errorLogFilePath);
+    }
+
+    var pvCloudAPI = {
+        test: function () {
+            return "SIMPLE SMOKE TEST";
+        },
+        /**
+         * Sends a value "asynchronously" using the file-based mechanism. 
+         * @param {string} label
+         * @param {string, integer, long, json simple object} value
+         * @param {string} type
+         * @param {datetime} captured_datetime
+         * @param {function} successCallback
+         * @param {function} errorCallback
+         * @param {function} finallyCallback
+         * @returns {undefined}
+         */
+        Write: function (baseURL, account_id, app_id, api_key, label, value, type, captured_datetime, successCallback, errorCallback, finallyCallback, debugOption) {
+            var wsURL = baseURL;
+            wsURL += "vse_add_value.php";
+            wsURL += '?app_id=' + app_id;
+            wsURL += '&api_key=' + api_key;
+            wsURL += '&account_id=' + account_id;
+            wsURL += '&label=' + label;
+            wsURL += '&value=' + value;
+            wsURL += '&type=' + type;
+            wsURL += '&captured_datetime=' + captured_datetime;
+
+            requestWrapper(wsURL, successCallback, errorCallback, finallyCallback);
+        },
+        /**
+         * Post File Mechanism
+         * @param {type} label
+         * @param {type} file_path
+         * @param {type} captured_datetime
+         * @param {type} successCallback
+         * @param {type} errorCallback
+         * @param {type} finallyCallback
+         * @returns {undefined}
+         */
+        PostFile: function (label, file_path, captured_datetime, successCallback, errorCallback, finallyCallback) {
+            var wsURL = baseURL += "vse_add_file.php";
+            log(wsURL);
+
+            log("CALLING REQUEST WRAPPER POST--------------------------------------------");
+
+            requestWrapperPost(wsURL, successCallback, errorCallback, finallyCallback, label, file_path, captured_datetime);
+
+        },
+        /**
+         * Requests a last_value from pvCloud in an asyncrhouous fashion using the file mechanism for the given label (or any label if not provided)
+         * @param {string} label
+         * @param {function} successCallback
+         * @param {function} errorCallback
+         * @param {function} finallyCallback
+         * @returns {undefined}
+         */
+        Read: function (label, successCallback, errorCallback, finallyCallback) {
+            log("consoleAPI.Read('" + label + "')");
+            var wsURL = baseURL;
+            wsURL += "vse_get_value_last.php";
+            wsURL += '?app_id=' + app_id;
+            wsURL += '&api_key=' + api_key;
+            wsURL += '&account_id=' + account_id;
+            wsURL += '&optional_label=' + label;
+            requestWrapper(wsURL, successCallback, errorCallback, finallyCallback);
+        },
+        /**
+         * Reads the results of an request operation (file determined by tag/label) and prints its value to the console.
+         * @param {string} label the tag for determining the file name.
+         * @returns {undefined}
+         */
+        Check: function (label) {
+            log("consoleAPI.Check('" + label + "')");
+            readResultFromFile(label, function (err, data) {
+                if (err) {
+                    errorLog(err);
+                    console.log("PVCLOUD_ERROR");
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+    };
+
+    exports.pvcloudAPI = pvCloudAPI;
+
 })();
--- pvcloud_npm/pvcloud_lib/spec/testSpec.js
+++ pvcloud_npm/pvcloud_lib/spec/testSpec.js
@@ -1,23 +1,75 @@
+var request = require("request");
 var pvcloud = require("../index.js");
 pvcloud = pvcloud.pvcloudAPI;
 
-console.log(pvcloud);
-
 describe("pvCloud API Object", function () {
+    info("");
+    info("API OBJECT TEST");
+
+    info("pvCloud API Object");
+    info(pvcloud);
     it("Must not empty", function () {
         expect(pvcloud).not.toBe(undefined);
     });
-
     it("Must have the minimal API functions declared", function () {
-        expect(pvcloud.read).not.toBe(undefined);
-        expect(pvcloud.write).not.toBe(undefined);
-        expect(pvcloud.post_file).not.toBe(undefined);
-        expect(pvcloud.check).not.toBe(undefined);
+        expect(pvcloud.Read).not.toBe(undefined);
+        expect(pvcloud.Write).not.toBe(undefined);
+        expect(pvcloud.PostFile).not.toBe(undefined);
+        expect(pvcloud.Check).not.toBe(undefined);
     });
-
     it("Must run Test Point Properly", function () {
         var smoketestResult = pvcloud.test();
-        console.log(smoketestResult);
-        expect(smoketestResult).toBe("THIS WAS JUST A TEST");
+        expect(smoketestResult).toBe("SIMPLE SMOKE TEST");
     });
 });
+
+describe("pvCloud Library Async WRITE Call", function () {
+    info("");
+    info("WRITE CALL TEST");
+    var callResponse;
+    beforeEach(function (done) {
+        var baseURL = "https://costaricamakers.com/pvcloud_pre/backend/";
+        var account_id = 5;
+        var app_id = 9;
+        var api_key = '8f5fb6fae58b9f597b2a3ccb8019966914661867';
+        var label = "test";
+        var value = "VALUE";
+        var type = "STRING";
+        var captured_datetime = "2016-01-01";
+        var debugOption = false;
+        var successCallback = function (response) {
+            info("Success Callback Reached!");
+            callResponse = response;
+        };
+        var errorCallback = function (response) {
+            info("Error Callback Reached!");
+            callResponse = response;
+        };
+        var finallyCallback = function (response) {
+            info("Finally Callback Reached!");
+            done();
+        };
+
+        pvcloud.Write(baseURL, account_id, app_id, api_key, label, value, type, captured_datetime, successCallback, errorCallback, finallyCallback, debugOption);
+    });
+
+    it("should be able to get a defined response", function (done) {
+        expect(callResponse).not.toBe(undefined);
+        expect(callResponse.body).not.toBe(undefined);
+
+        info("EXTRACTING DATA FROM RESPONSE...");
+        data = JSON.parse(callResponse.body);
+        expect(data).not.toBe(undefined);
+
+        info(data);
+        expect(data.entry_id).toBeGreaterThan(1);
+
+        done();
+    });
+});
+
+
+function info(value) {
+    console.log(value);
+    console.log("----------------------------------");
+}
