a594f58de6bb06978143e78813d2ba5c35264675
Jose Nunez <jose.nunez@intel.com>
2016-04-22 14:55:00 -0600

    initial working version from YO

---
+++ pvcloud_npm/pvcloud_cli_web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvcloudCliWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pvcloudCliWebApp
+ */
+angular.module('pvcloudCliWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ pvcloud_npm/pvcloud_cli_web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pvcloudCliWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pvcloudCliWebApp
+ */
+angular.module('pvcloudCliWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
